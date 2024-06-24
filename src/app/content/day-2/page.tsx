da700ac5af998882db154ec9112db742227d9e24
Tristan <tristangee@gmail.com>
2024-06-24 15:22:41 -0700

    cleaned up + added day 2

---
+++ src/app/content/day-2/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Day2() {
+    return (
+        <div className='w-screen h-screen'>
+            <iframe src="/content/pdf/day-2.pdf" className='w-full h-full' />
+        </div>
+    )
+}
