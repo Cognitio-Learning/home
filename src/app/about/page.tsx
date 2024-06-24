e11fc50f10fb3488950ecfd25151df4c816a10bb
Tristan <tristangee@gmail.com>
2024-06-24 15:22:30 -0700

    placeholder pages for now

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function Python() {
+    return (
+        <div className="flex justify-center items-center h-screen w-screen text-white">
+            <h1 className="text-4xl">hello</h1>
+        </div>
+    )
+}
