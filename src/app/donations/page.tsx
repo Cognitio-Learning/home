6eaf4f138fb91b1f08fd26ee792197cf60856680
Tristan <tristangee@gmail.com>
2024-07-02 13:23:48 -0700

    add donations page

---
+++ src/app/donations/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Contact() {
+    return (
+        <div className="flex justify-center items-center h-screen w-screen text-white ">
+            <h1 className="text-4xl font-bold">Coming soon...</h1>
+        </div>
+    )
+}
