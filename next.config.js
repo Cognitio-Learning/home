37b1b646e7cdb7b4f7206dc87260572137db8cef
Tristan <tristangee@gmail.com>
2024-07-02 13:23:31 -0700

    config + tailwind typography

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const withMDX = require('@next/mdx')()
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // Configure `pageExtensions` to include MDX files
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    // Optionally, add any other Next.js config below,
+    experimental: {
+        mdxRs: true
+    }
+}
+
+module.exports = withMDX(nextConfig)
