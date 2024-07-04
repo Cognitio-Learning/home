59712df7bdee1d32055ff96762f694df46b09c9d
Tristan <tristangee@gmail.com>
2024-07-03 22:17:44 -0700

    add temp content

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link';
 import React from 'react';
 
 export default function Contact() {
     return (
         <div className="flex justify-center items-center h-screen w-screen text-white ">
-            <h1 className="text-4xl font-bold">Coming soon...</h1>
+            <h1 className="text-4xl font-bold">Temporary form <Link href="/about" className='text-blue-500 underline'>here.</Link></h1>
         </div>
     )
 }
